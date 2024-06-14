a50ac34d8b2be6fb2f304c0e654c0ffd0f63787b
Oğuzhan Yıldırım <yildirimogcod@gmail.com>
2024-06-14 19:51:44 +0300

    installed react react-router tailwindcss and created header

---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex max-w-6xl mx-auto items-center justify-between p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-2xl flex flex-wrap gap-2">
+            <span className="text-slate-500">YILDIRIM</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="focus:outline-none bg-transparent w-24 sm:w-64"
+          />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline ">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline ">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-up">
+            <li className=" text-slate-700 hover:underline"> Sign in</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
