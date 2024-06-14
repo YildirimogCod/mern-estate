816dfb9c91a37c49346bdc7403342fab967d3fbe
Oğuzhan Yıldırım <yildirimogcod@gmail.com>
2024-06-14 20:23:22 +0300

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { dot } from "node:test/reporters";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connect");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 app.listen(3000, () => {
   console.log("server is running on port 3000!!");
