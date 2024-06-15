34afc0c0fbe58b76de7eebd365cd5c14873eef1c
Oğuzhan Yıldırım <yildirimogcod@gmail.com>
2024-06-15 13:32:46 +0300

    created ssign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   const { userName, email, password } = req.body;
   const hashedPassword = await bcryptjs.hashSync(password, 10);
@@ -13,6 +15,31 @@ export const signup = async (req, res, next) => {
     res.status(201).json({
       message: "User created successfully",
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) {
+      next(errorHandler(404, "User not found"));
+    }
+    const validPassword = await bcryptjs.compareSync(
+      password,
+      validUser.password
+    );
+    if (!validPassword) {
+      next(errorHandler(401, "Wrong credential!"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: newPass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
