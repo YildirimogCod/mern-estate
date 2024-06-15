bd62b545994e6df1d26a78e2c8f65785fbddfd3f
Oğuzhan Yıldırım <yildirimogcod@gmail.com>
2024-06-15 13:45:35 +0300

    fixed password bugs

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,18 +3,12 @@ import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
-  const { userName, email, password } = req.body;
-  const hashedPassword = await bcryptjs.hashSync(password, 10);
-  const newUser = new User({
-    userName,
-    email,
-    password: hashedPassword,
-  });
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json({
-      message: "User created successfully",
-    });
+    res.status(201).json("User created successfully!");
   } catch (error) {
     next(error);
   }
@@ -23,19 +17,12 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
-    const validUser = await User.findOne({ email: email });
-    if (!validUser) {
-      next(errorHandler(404, "User not found"));
-    }
-    const validPassword = await bcryptjs.compareSync(
-      password,
-      validUser.password
-    );
-    if (!validPassword) {
-      next(errorHandler(401, "Wrong credential!"));
-    }
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const { password: newPass, ...rest } = validUser._doc;
+    const { password: pass, ...rest } = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
