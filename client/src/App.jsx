26d8629dac1be651fcdb4c57c2cf9b54d50f51f6
Oğuzhan Yıldırım <yildirimogcod@gmail.com>
2024-06-20 15:54:30 +0300

    fixed some bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 export default function App() {
   return (
     <>
@@ -18,6 +19,7 @@ export default function App() {
 
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/listing/:listingId" element={<Listing />} />
           <Route element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
             <Route path="/create-listing" element={<CreateListing />} />
